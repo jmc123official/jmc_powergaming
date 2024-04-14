860b8a94c42d77f6858cfa3f7b8bbd0ab9949483
John Cherian <120570424+jmc123official@users.noreply.github.com>
2024-04-15 02:31:04 +0530

    Update config.lua

---
+++ shared/config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config.burn = true
+Config.conehead = true
+Config.crash = true
+Config.drunk = true
+Config.hump = true
+Config.poop = true
+Config.showbbs = true
+Config.slap = true
+Config.surrender = true
+Config.sucide = true
+Config.wstick = true
+
+--Discord
+Config.log = true
+Config.Webhook = ""
+Config.BotName = ""
+Config.Image = ""
